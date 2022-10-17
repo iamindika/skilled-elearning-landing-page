e9723bd663290c5997096d593805a8f134faf958
iamindika <ikolamba2@gmail.com>
2022-10-17 01:10:24 -0400

    add data for course component resourcing

---
+++ src/db/data.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    id: 1,
+    icon: "icon-animation.svg",
+    title: "Animation",
+    description: "Learn the latest animation techniques to create stunning motion design and captivate your audience.",
+  },
+  {
+    id: 2,
+    icon: "icon-design.svg",
+    title: "Design",
+    description: "Create beautiful, usable interfaces to help shape the future of how the web looks.",
+  },
+  {
+    id: 3,
+    icon: "icon-photography.svg",
+    title: "Photography",
+    description: "Explore critical fundamentals like lighting, composition, and focus to capture exceptional photos.",
+  },
+  {
+    id: 4,
+    icon: "icon-crypto.svg",
+    title: "Crypto",
+    description: "All you need to know to get started investing in crypto. Go from beginner to advanced with this 54 hour course.",
+  },
+  {
+    id: 5,
+    icon: "icon-business.svg",
+    title: "Business",
+    description: "A step-by-step playbook to help you start, scale, and sustain your business without outside investment.",
+  },
+]
