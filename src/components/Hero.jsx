c8277927c5febb56af30b86ac4a1523d45d29bb6
iamindika <ikolamba2@gmail.com>
2022-10-17 03:00:33 -0400

    utilize use window dimensions hook to retrieve images responsively

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,32 +1,68 @@
+import useWindowDimensions from "../hooks/useWindowDimensions";
 import { getImage } from "../utilities/helpers";
 
-const Hero = () => (
-  <header className="hero container">
-    <div className="hero-content">
-      <h1 className="hero-title">
-        Maximize skill, minimize budget
-      </h1>
+const RESPONSIVE_HERO_IMG = {
+  X_SMALL: {
+    size: 0,
+    file: "image-hero-mobile.png"
+  }, 
+  SMALL: {
+    size: 500,
+    file: "image-hero-mobile@2x.png"
+  }, 
+  MEDIUM: {
+    size: 768,
+    file: "image-hero-desktop.png"
+  }, 
+  LARGE: {
+    size: 1440,
+    file: "image-hero-desktop@2x.png"
+  }
+}
 
-      <p className="hero-text">
-        Our modern courses across a range of in-demand skills will give you the knowledge you need to live the life you want.
-      </p>
+const Hero = () => {
+  const { width } = useWindowDimensions();
 
-      <a 
-        className="hero-btn"
-        href="https://www.frontendmentor.io/profile/iamindika"
-        target="_blank"
-        rel="noopener noreferrer"
-      >Get Started</a>
-    </div>
+  let heroImage;
 
-    <div className="hero-image-wrapper container">
-      <img
-        className="hero-image"
-        src={getImage('image-hero-mobile.png')}
-        alt="hero"
-      />
-    </div>
-  </header>
-);
+  if (width >= RESPONSIVE_HERO_IMG.LARGE.size) {
+    heroImage = RESPONSIVE_HERO_IMG.LARGE.file;
+  } else if (width >= RESPONSIVE_HERO_IMG.MEDIUM.size) {
+    heroImage = RESPONSIVE_HERO_IMG.MEDIUM.file;
+  } else if (width >= RESPONSIVE_HERO_IMG.SMALL.size) {
+    heroImage = RESPONSIVE_HERO_IMG.SMALL.file;
+  } else if (width >= RESPONSIVE_HERO_IMG.X_SMALL.size) {
+    heroImage = RESPONSIVE_HERO_IMG.X_SMALL.file;
+  } 
+
+  return (
+    <header className="hero container">
+      <div className="hero-content">
+        <h1 className="hero-title">
+          Maximize skill, minimize budget
+        </h1>
+
+        <p className="hero-text">
+          Our modern courses across a range of in-demand skills will give you the knowledge you need to live the life you want.
+        </p>
+
+        <a 
+          className="hero-btn"
+          href="https://www.frontendmentor.io/profile/iamindika"
+          target="_blank"
+          rel="noopener noreferrer"
+        >Get Started</a>
+      </div>
+
+      <div className="hero-image-wrapper container">
+        <img
+          className="hero-image"
+          src={getImage(heroImage)}
+          alt="hero"
+        />
+      </div>
+    </header>
+  );
+}
 
 export default Hero;
