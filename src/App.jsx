c57aae58c6e8bdf1ebe9558a432a49fffc26a193
iamindika <ikolamba2@gmail.com>
2022-10-16 22:25:50 -0400

    create navbar component, define styles and render

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Navbar from "./components/Navbar";
+
 function App() {
  return (
-    <div>
-      Skilled E-Learning Landing Page
-    </div>
+    <>
+      <Navbar />
+    </>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => (
+  <nav className="nav container">
+    <span className="nav-logo">skilled</span>
+    <input 
+      className="nav-btn"
+      type="button" 
+      value="Get Started" 
+    />
+  </nav>
+);
+
+export default Navbar;
