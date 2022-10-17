837d9e5f18645cc6f95ecde1ee4bbfd94a07864e
iamindika <ikolamba2@gmail.com>
2022-10-17 02:10:48 -0400

    create start button component

---
+++ src/components/StartButton.jsx
@@ -0,0 +1,10 @@
+const StartButton = () => (
+  <a 
+      className="btn"
+      href="https://www.frontendmentor.io/profile/iamindika"
+      target="_blank"
+      rel="noopener noreferrer"
+    >Get Started</a>
+);
+
+export default StartButton;
