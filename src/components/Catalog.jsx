d15da42c16e9ec8211ca389fba0b2980f4d19df2
iamindika <ikolamba2@gmail.com>
2022-10-17 01:09:43 -0400

    create catalog component, define styles and render

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -1,8 +1,20 @@
+import Course from "./Course";
+import courseList from "../db/data";
+
 const Catalog = () => (
-  <section className="catalog container">
-    <h2 className="catalog-title">
-      Check out our most popular courses!
-    </h2>
+  <section className="catalog">
+    <div className="container">
+      <h2 className="catalog-title">
+        Check out our most popular courses!
+      </h2>
+
+      {courseList.map((course) => (
+        <Course
+          key={course.id}
+          {...course}
+        />
+      ))}
+    </div>
   </section>
 );
 
